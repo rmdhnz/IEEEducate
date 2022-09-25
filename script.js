1b39c5716764ba6e7ec7e4ef37ca4065cc992496
rmdhnz <ramadhanfadhly2@gmail.com>
2022-09-26 03:30:36 +0700

    membuat API form

---
+++ script.js
@@ -0,0 +1,21 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyIMmyQ3fbTfBcJsvI_Su8m4_Kj47JYJh7tfFhIRmXJmfxxDXOpxscEj1QxP5Mooffi/exec";
+const form = document.forms["contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      myAlert.classList.toggle("d-none");
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
