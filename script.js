865da55b7169d99b05d84687ccafe6406bec9501
rmdhnz <ramadhanfadhly2@gmail.com>
2022-09-27 10:25:08 +0700

    menambah WAG

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const form = document.forms["contact-form"];
 const btnKirim = document.querySelector(".btn-kirim");
 const btnLoading = document.querySelector(".btn-loading");
 const myAlert = document.querySelector(".my-alert");
-
+const section = document.querySelectorAll("section");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   btnLoading.classList.toggle("d-none");
@@ -16,6 +16,10 @@ form.addEventListener("submit", (e) => {
       btnKirim.classList.toggle("d-none");
       myAlert.classList.toggle("d-none");
       form.reset();
+      for (let i = 0; i < section.length; ++i) {
+        section[i].classList.toggle("d-none");
+      }
+      document.querySelector(".overlayer").classList.toggle("d-none");
     })
     .catch((error) => console.error("Error!", error.message));
 });
